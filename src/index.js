75f9dfa27beab6183297d35b420a4e7678690a0c
Muhammad Ishrath <ishrathmuhammadu@gmail.com>
2024-10-29 00:56:55 +0530

    created the folder structure based on the System architecture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+
+// Define the port
+const port = process.env.PORT || 3000;
+
+// Middleware (optional, but commonly used)
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('M World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
